574d9866044d84d2a7d25f79925eb9c103ddca84
Kunal <patelkr07@gmail.com>
2019-05-18 00:02:29 -0500

    added screenshots

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mysql-1
+
+screen1.png
+
+
+screen2.png
+
+screen3.png
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 
     user: "root",
 
-    password: "H01046641",
+    password: "",
     database: "bamazonDB"
 });
 
@@ -17,6 +17,7 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
     console.log("\n")
     queryBamazon();
+    start();
     
 })
 
@@ -39,6 +40,56 @@ function start() {
     })
     .then(function(answer) {
         if(answer.askID === "1")
-    }
+        { 
+            console.log("How much Car Wash Soap would you like to buy?")
+        }
+        else if(answer.askID === "2")
+        {
+            console.log("How many Wristwatches would you like to buy?")
+        }
+        else if(answer.askID === "3")
+        {
+            console.log("How many Televisions would you like to buy?")
+        }
+        else if(answer.askID === "4")
+        {
+            console.log("How many Swimsuits would you like to buy?")
+        }
+        else if(answer.askID === "5")
+        {
+            console.log("How many Lipsticks would you like to buy?")
+        }
+        else if(answer.askID === "6")
+        {
+            console.log("How many Shoes would you like to buy?")
+        }
+        else if(answer.askID === "7")
+        {
+            console.log("How many Laptops would you like to buy?")
+        }
+        else if(answer.askID === "8")
+        {
+            console.log("How many Bicycles would you like to buy?")
+        }
+        else if(answer.askID === "9")
+        {
+            console.log("How many Pianos would you like to buy?")
+        }
+        else if(answer.askID === "10")
+        {
+            console.log("How many Spark Plugs would you like to buy?")
+        }
+
+    })
 }
+
+//function for inquirer prompt 2 should be after first prompt, also need drier code to not repeat 10 timess
+
+//console.log ("You've bought _____________")
+
+    //then function for subtracting 1 from the quantity
+
+
+
+
 
